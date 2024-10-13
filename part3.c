3beaa1233a19d4be95f3fa360b537538acfecfb0
deathknight5302 <1601047525@qq.com>
2024-10-13 09:59:32 +0800

    part3.c

---
+++ part3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int lena=0,lenb=0;
+    char a[128]={0},b[128]={0},c[128]={0};
+    scanf("%s",&a);
+    scanf("%s",&c);
+    scanf("%s",&b);
+    lena=strlen(a);
+    lenb=strlen(b);
+    for(int i=0;i<=lena-1;i++){
+        printf("%c",a[i]);
+    }
+    printf("\n");
+    for(int i=0;i<=lenb-1;i++){
+        if(b[i]=='('||b[i]==')'){
+            continue;
+        }
+        printf("%c",b[i]);
+    }
+    printf("\n");
+    printf("%c",c[0]);
+    return 0;
+}
